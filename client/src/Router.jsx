1382dd6eb0b990e7d9bff79935a8c003d735ba08
Ilse Löhr <ilse.loehr@s2023.tu-chemnitz.de>
2024-05-21 19:16:22 +0200

    add Rounting to site and pages Impressum, Home as LogIn

---
+++ client/src/Router.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home.jsx'
+import LogIn from "./LogIn.jsx";
+import Impressum from './Impressum.jsx'
+import NoPage from "./NoPage.jsx";
+import './App.css'
+
+function Router() {
+
+  return (
+     <BrowserRouter>
+        <Routes>
+
+            {/* Home */}
+            <Route path="/" element={<Home />} />
+
+            {/* Home */}
+            <Route index element={<Home />} />
+
+            {/* Impressum */}
+            <Route path="impressum" element={<Impressum />} />
+
+             {/* LogIn */}
+            <Route path="anmelden" element={<LogIn />} />
+
+            {/* 404 Page */}
+            <Route path="*" element={<NoPage />} />
+
+
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default Router
