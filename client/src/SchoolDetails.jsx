32ca1fc8fbb410cd3345cd08bd1fc00d80c03681
Ilse Löhr <ilse.loehr@s2023.tu-chemnitz.de>
2024-05-28 14:32:17 +0200

    excluded school informations in component SchoolDetails and add further icon colors

---
+++ client/src/SchoolDetails.jsx
@@ -0,0 +1,39 @@
+import 'leaflet/dist/leaflet.css'
+import { Marker, Popup } from 'react-leaflet'
+import { Icon } from 'leaflet';
+import POIRed from './assets/POI_red_400.png'
+import PropTypes from 'prop-types';
+
+
+export default function SchoolDetails({ school, index }) {
+
+  //customIcon for schools
+  const customIcon = new Icon({
+    iconUrl: POIRed,
+    iconSize: [25, 25],
+    iconAnchor: [10, 25],
+  })
+
+ 
+  return (
+        <Marker
+        key={index}
+        icon={customIcon}
+        position={[school.Y, school.X]} >
+        <Popup>
+            <strong>{school.BEZEICHNUNG}</strong><br />
+            Art: {school.ART},<br />
+            Tel.: {school.TELEFON},<br />
+            E-Mail: {school.EMAIL}, <br />
+            Straße: {school.STRASSE},<br />
+            Plz: {school.PLZ},
+            Ort: {school.ORT}
+        </Popup>
+        </Marker>
+  );
+}
+
+SchoolDetails.propTypes = {
+  school: PropTypes.any.isRequired,
+  index: PropTypes.number.isRequired
+}
